ec34930dc27baf67ef1d91765c523c58ec5031e6
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-05-08 23:05:58 +0200

    triggerDebug
    lastPhase

--- 4_hasznalatiEsetek_2_triggerek.sql
+++ 4_hasznalatiEsetek_2_triggerek.sql
@@ -92,86 +92,6 @@ END;
 //
 DELIMITER ;
 
--- TRIGGER létrehozása a Harcol táblához
-DELIMITER //
-
-CREATE TRIGGER check_harc_kovetelmeny_and_szorny_legyozese
-BEFORE INSERT ON Harcol
-FOR EACH ROW
-BEGIN
-    DECLARE Karakter_potencial FLOAT;
-    DECLARE szorny_potencial FLOAT;
-    DECLARE Karakter_tamadas FLOAT;
-    DECLARE szorny_tamadas FLOAT;
-    DECLARE Karakter_kritikus_tamadas FLOAT;
-    DECLARE szorny_kritikus_tamadas FLOAT;
-    DECLARE gyoztesId INT;
-    DECLARE szorny_felsz_id INT;
-    DECLARE felszereles_id INT;
-    DECLARE tapasztalatPontAd INT;
-    DECLARE aranyAd INT;
-    DECLARE csoportTagokSzama INT; -- új változó a csoporttagok számához
-
-    -- Kérjük le a játékos harci potenciálját
-    SELECT (szint * (eletero + sebzes) * (1 + RAND() * 0.4 - 0.2)) INTO Karakter_potencial
-    FROM Karakter
-    WHERE id = NEW.Karakter1Id;
-
-    -- Kérjük le a szörny harci potenciálját
-    SELECT (szint * (eletero + sebzes) * (1 + RAND() * 0.4 - 0.2)) INTO szorny_potencial
-    FROM Szorny
-    WHERE id = NEW.szornyId;
-
-    -- Kiszámoljuk a támadásokat
-    SET Karakter_tamadas = Karakter_potencial * (1 + RAND() * 0.4 - 0.2);
-    SET szorny_tamadas = szorny_potencial * (1 + RAND() * 0.4 - 0.2);
-
-    -- Kiszámoljuk a kritikus támadásokat
-    SET Karakter_kritikus_tamadas = Karakter_tamadas * (1 + RAND() * 0.4 - 0.2);
-    SET szorny_kritikus_tamadas = szorny_tamadas * (1 + RAND() * 0.4 - 0.2);
-
-    -- Kiválasztjuk a győztest
-    IF Karakter_kritikus_tamadas > szorny_kritikus_tamadas THEN
-        SET gyoztesId = NEW.Karakter1Id;
-    ELSE
-        SET gyoztesId = NEW.szornyId;
-    END IF;
-
-    -- Ha a győztes a játékos, akkor növeljük a szörny tapasztalati pontjait, egyébként a játékosét
-    IF gyoztesId = NEW.Karakter1Id THEN
-        SET tapasztalatPontAd = NEW.tapasztalatPont * 0.8;
-        SET aranyAd = NEW.arany * 0.8;
-    ELSE
-        SET tapasztalatPontAd = NEW.tapasztalatPont * 1.2;
-        SET aranyAd = NEW.arany * 1.2;
-    END IF;
-
-    -- Beállítjuk a győztes játékost vagy a szörnyet
-    SET szorny_felsz_id = NULL;
-    SET felszereles_id = NULL;
-
-    IF gyoztesId = NEW.Karakter1Id THEN
-        SET szorny_felsz_id = NEW.szorny_felsz_id;
-    ELSE
-        SET felszereles_id = NEW.felszerelesId;
-    END IF;
-
-    -- Ellenőrizzük, hogy a játékos csoportja csak 4 tagból álljon
-    SELECT COUNT(*) INTO csoportTagokSzama
-    FROM Karakter
-    WHERE csoportId = NEW.Karakter1Id;
-
-    IF csoportTagokSzama > 4 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Egy csoportban nem lehet több mint 4 játékos.';
-    END IF;
-
-    -- Beillesztjük az új győzelmet a harci naplóba
-    INSERT INTO Harci_Naplo (Karakter1Id, szornyId, gyoztesId, tapasztalatPont, arany, szorny_felsz_id, felszerelesId)
-    VALUES (NEW.Karakter1Id, NEW.szornyId, gyoztesId, tapasztalatPontAd, aranyAd, szorny_felsz_id, felszereles_id);
-END;
-//
-DELIMITER ;
 
 
 -- TRIGGER létrehozása a Karakter táblához
@@ -303,56 +223,6 @@ END;
 //
 DELIMITER ;
 
--- TRIGGER létrehozása a Karakter táblához
-DELIMITER //
-
-CREATE TRIGGER csoportbol_kilep
-BEFORE DELETE ON Csoport
-FOR EACH ROW
-BEGIN
-    DELETE FROM Csoport
-    WHERE KarakterId = OLD.KarakterId AND csoportId = OLD.csoportId;
-END;
-//
-DELIMITER ;
-
--- TRIGGER létrehozása a Karakter táblához
-CREATE TRIGGER csoportba_belep
-BEFORE INSERT ON Csoport_Tag
-FOR EACH ROW
-BEGIN
-    DECLARE tagokSzama INT;
-    
-    -- Növeljük a csoport tagjainak számát
-    SELECT COUNT(*) INTO tagokSzama
-    FROM Csoport_Tag
-    WHERE csoportId = NEW.csoportId;
-
-    UPDATE Csoport
-    SET tagokSzama = tagokSzama
-    WHERE id = NEW.csoportId;
-END;
-//
-DELIMITER ;
-
-
--- TRIGGER létrehozása a Karakter táblához
-DELIMITER //
-
-CREATE TRIGGER csoportba_belep
-BEFORE INSERT ON Karakter
-FOR EACH ROW
-BEGIN
-    -- Ellenőrizzük, hogy az újonnan beszúrt játékosnak van-e csoportja
-    IF NEW.csoportId IS NOT NULL THEN
-        -- Frissítjük a csoport tagjainak számát
-        UPDATE Csoport
-        SET tagokSzama = tagokSzama + 1
-        WHERE id = NEW.csoportId;
-    END IF;
-END;
-//
-DELIMITER ;
 
 -- TRIGGER létrehozása a Karakter táblához
 DELIMITER //
@@ -380,52 +250,7 @@ END;
 DELIMITER ;
 
 
--- TRIGGER létrehozása a BoltFelszereles táblához
-DELIMITER //
 
-CREATE TRIGGER felszereles_elad
-BEFORE INSERT ON BoltFelszereles
-FOR EACH ROW
-BEGIN
-    -- Ellenőrizzük, hogy a felszerelés eladható-e
-    IF (SELECT felszerelesFelveve FROM KarakterFelszereles WHERE KarakterId = NEW.KarakterId AND felszerelesId = NEW.felszerelesId) = FALSE THEN
-        -- Beszúrjuk az eladott felszerelést a BoltFelszereles táblába
-        INSERT INTO BoltFelszereles (boltId, felszerelesId)
-        VALUES (NEW.boltId, NEW.felszerelesId);
-        
-        -- A felszerelés árát a játékos kapja meg
-        UPDATE Karakter
-        SET arany = arany + (SELECT ar FROM Felszereles WHERE id = NEW.felszerelesId)
-        WHERE id = NEW.KarakterId;
-    END IF;
-END;
-//
-DELIMITER ;
 
--- TRIGGER létrehozása a BoltFelszereles táblához
-DELIMITER //
 
-CREATE TRIGGER felszereles_vasarol
-BEFORE INSERT ON BoltFelszereles
-FOR EACH ROW
-BEGIN
-    -- Ellenőrizzük, hogy a játékosnak megfelelő-e a szintje a felszerelés vásárlásához
-    DECLARE Karakter_szint INT;
-    DECLARE felszereles_min_szint INT;
-    DECLARE felsz_id INT;
-
-    -- Kérjük le a játékos aktuális szintjét és a felszerelés minimális szintjét
-    SELECT NEW.KarakterId INTO Karakter_szint;
-    SELECT minimumSzint INTO felszereles_min_szint
-    FROM Felszereles
-    WHERE id = NEW.felszerelesId;
-
-    -- Ellenőrizzük, hogy a játékos szintje megfelel-e a felszerelés vásárlásához
-    IF Karakter_szint < felszereles_min_szint THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'A játékos szintje nem megfelelő a felszerelés vásárlásához.';
-    END IF;
-END;
-//
-DELIMITER ;
 
